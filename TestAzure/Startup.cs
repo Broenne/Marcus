6cd0265bb22d1c376e2b9a29988dd0367af03b5d
Broenne <marcus.broenstrup@gmail.com>
2016-02-17 13:34:24 +0100

    no message

---
+++ TestAzure/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TestAzure.Startup))]
+
+namespace TestAzure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
